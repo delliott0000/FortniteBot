1f62c28b287d830bf5187135decf67a00e392835
Delliott <dlelliott100@outlook.com>
2023-10-20 04:33:29 +0100

    command decorators

---
+++ core/decorators.py
@@ -0,0 +1,72 @@
+from core.bot import FortniteBot
+
+from discord import app_commands, Interaction
+
+
+_Int = Interaction[FortniteBot]
+
+
+def is_not_blacklisted():
+
+    async def predicate(interaction: _Int) -> bool:
+        uid = interaction.user.id
+
+        _blacklisted = await interaction.client.database_client.is_blacklisted(uid)
+        if _blacklisted is True:
+            raise app_commands.CheckFailure('You have been blacklisted from using this feature.')
+
+        return True
+
+    return app_commands.check(predicate)
+
+
+def is_not_logged_in():
+
+    async def predicate(interaction: _Int) -> bool:
+        uid = interaction.user.id
+
+        _logged_in = bool(interaction.client.get_auth_session(uid))
+        if _logged_in is True:
+            raise app_commands.CheckFailure('You must be logged out to use this feature.')
+        return True
+
+    return app_commands.check(predicate)
+
+
+def is_logged_in():
+
+    async def predicate(interaction: _Int) -> bool:
+        uid = interaction.user.id
+
+        _logged_in = bool(interaction.client.get_auth_session(uid))
+        if _logged_in is False:
+            raise app_commands.CheckFailure('You must be logged in to use this feature.')
+        return True
+
+    return app_commands.check(predicate)
+
+
+def is_premium():
+
+    async def predicate(interaction: _Int) -> bool:
+        uid = interaction.user.id
+
+        _premium = await interaction.client.database_client.is_premium(uid)
+        if _premium is False:
+            raise app_commands.CheckFailure('You must be a premium user to use this feature.')
+
+        return True
+
+    return app_commands.check(predicate)
+
+
+def non_premium_cooldown():
+
+    async def predicate(interaction: _Int) -> app_commands.Cooldown | None:
+        uid = interaction.user.id
+
+        _premium = await interaction.client.database_client.is_premium(uid)
+        if _premium is False:
+            return app_commands.Cooldown(1, 15)
+
+    return app_commands.checks.dynamic_cooldown(predicate)
