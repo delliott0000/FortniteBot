b0c3ad6300204961ecf6ba301de24a1b8526c79a
Delliott <dlelliott100@outlook.com>
2023-10-20 14:46:07 +0100

    base fortnite item impl

---
+++ fortnite/base.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from weakref import ref
+
+if TYPE_CHECKING:
+    from core.account import PartialEpicAccount, FullEpicAccount
+
+    Account = PartialEpicAccount | FullEpicAccount
+    MaybeAccount = Account | None
+
+
+class BaseEntity:
+
+    def __init__(
+            self,
+            account: MaybeAccount,
+            item_id: str,
+            template_id: str
+    ) -> None:
+        try:
+            self._account = ref(account)
+        except TypeError:
+            self._account = lambda: None
+
+        self.item_id: str = item_id
+        self.template_id: str = template_id
+
+    @property
+    def account(self) -> MaybeAccount:
+        return self._account()
