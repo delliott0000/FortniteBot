3d0b8680f712760adb630d63f96b93f9295e7809
Delliott <dlelliott100@outlook.com>
2023-10-26 21:10:26 +0100

    reverse last commit; bad idea

--- components/embed.py
+++ components/embed.py
@@ -1,8 +1,4 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from core.decorators import FortniteInteraction
 
 from discord import Embed
 
@@ -24,15 +20,6 @@ class EmbedField:
 class CustomEmbed(Embed):
 
     __slots__ = ()
-
-    def __init__(
-        self,
-        interaction: FortniteInteraction,
-        title: str | None = None,
-        description: str | None = None
-    ) -> None:
-        colour = interaction.client.colour(interaction.guild)
-        super().__init__(title=title, description=description, colour=colour)
 
     def append_field(self, field: EmbedField) -> CustomEmbed:
         return self.add_field(name=field.name, value=field.value, inline=field.inline)
